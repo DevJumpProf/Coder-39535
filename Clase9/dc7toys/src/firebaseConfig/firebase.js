160eaeabc690faff91608a8ce75f019dabc5aab7
Gabriel Muñoz <gabriel.munoz@arbusta.net>
2023-02-09 16:36:57 -0300

    Entregable

--- Clase9/dc7toys/src/firebaseConfig/firebase.js
+++ Clase9/dc7toys/src/firebaseConfig/firebase.js
@@ -7,15 +7,7 @@ import {getFirestore} from "@firebase/firestore"
 // Your web app's Firebase configuration
 
 // aca va tu codigo de FIREBASE
-const firebaseConfig = {
-    apiKey: "AIzaSyBQaRFcpsSj3D4Ei9rcZiIp9pYn1gIiuT4",
-    authDomain: "crudcoder39535.firebaseapp.com",
-    projectId: "crudcoder39535",
-    storageBucket: "crudcoder39535.appspot.com",
-    messagingSenderId: "151504072588",
-    appId: "1:151504072588:web:f2be4d7436fadf17d230a9"
-  };
-  
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
