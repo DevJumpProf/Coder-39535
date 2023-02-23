64caa4aeeaf3db595a5e8fa2358db8169a3d60a9
Gabriel Muñoz <gabriel.munoz@arbusta.net>
2023-02-23 00:33:54 -0300

    Repaso

--- Clase12/codermovies/src/utils/httpCliente.js
+++ Clase12/codermovies/src/utils/httpCliente.js
@@ -4,7 +4,7 @@ export const get = (path)=>{
     return fetch (API+path,{
         headers:{
             Authorization:
-            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3M2I5NmRkYzI1NjU4YWQxN2M5MDQ0MGU3ZjYxYmQ2NyIsInN1YiI6IjYzZjY5NzFkMWYzMzE5MDA4NDNhYzQ5NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.AcIf3KJ97EjbNZB_54h8sZF6a_l8JzmeBjruFnMVTxs",
+            "Bearer TU API KEY",
             "Content-Type":"application/json;charset=utf-8",
 
         }
